aa1b0c652a89ed59ea3e5e841d140924ab629199
Nirmanov <vladislav.nirmanov@yandex.ru>
2022-12-15 08:54:37 +0300

    test

---
+++ crm-frontend/js/views/showall.js
@@ -0,0 +1,47 @@
+// function showall() {
+// let contactsList = document.querySelector('.clients__body-contacts');
+// console.log(contactsList);
+// // let contactItem = contactsList.querySelectorAll('.clients__body-tooltip');
+// // if (contactsList.length > 5) {
+// //   contactItem.classList.add('invisible')
+// // }
+
+// // if (client.contacts.length > 5) {
+//   // contactsAmount = 4;
+//   // let contactsOpen = contactsAmount;
+//   // createContacts();
+//   // const i = client.contacts.length - contactsOpen;
+
+// //   const contactItem = createElem(
+// //     "li",
+// //     {
+// //       classList: ["clients__table-body-contact"],
+// //     },
+// //     clientContactsList
+// //   );
+// //   const contactLinkLimit = createElem(
+// //     "button",
+// //     {
+// //       textContent: `+${i}`,
+// //       ariaLabel: "Показать остальные социальные сети",
+// //       classList: ["clients__table-body-contact-limit"],
+// //     },
+// //     contactItem
+// //   );
+
+// //   tippy(contactLinkLimit, {
+// //     content: "Показать оставшиеся контакты",
+// //   });
+
+// //   contactLinkLimit.addEventListener("click", () => {
+// //     contactsAmount = client.contacts.length;
+// //     clientContactsList.innerHTML = "";
+// //     createContacts();
+// //   });
+// // } else {
+// //   contactsAmount = client.contacts.length;
+// //   createContacts();
+// // }
+
+// }
+// showall();
